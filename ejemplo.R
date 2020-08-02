a809daeeed19816828eae359ef71d21ddd89aae4
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-02 01:02:14 -0500

    add example script

---
+++ ejemplo.R
@@ -0,0 +1,10 @@
+# Ejemplos de .Rprofile
+# options(digits = 3) Para evaluar este efecto corre esta operacion antes
+# y despues de cambiar el profile.
+1/10
+# library(tidyverse) Aqui se observa que se imprime el mensaje resultado de
+# hacer el library(). Si queremos omitir esto (aunque es mas riesgoso) se puede
+# usar options(defaultPackages = "tidyverse")
+# f <- dplyr::filter prueba este comando
+f(iris, Species == "versicolor")
+
